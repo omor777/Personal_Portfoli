c4300b125e2006168be5ce71be41c61a152c8d16
omor777 <bengalifact26@gmail.com>
2023-11-28 12:33:36 +0600

    experience section html css code complete

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const sideMenu = document.getElementById("side-menu");
 const toggleBtn = document.querySelector(".bar-container");
 const mainContent = document.querySelector(".main-content");
+const accordionHead = document.querySelectorAll(".accordion_head");
+const accordionBody = document.querySelectorAll(".accordion_body");
+const plus = document.querySelector(".plus");
+const minus = document.querySelector(".minus");
 
 toggleBtn.addEventListener("click", function () {
   sideMenu.classList.toggle("showWidth");
@@ -15,3 +19,27 @@ window.addEventListener("scroll", function () {
     toggleBtn.classList.remove("active");
   }
 });
+
+// accordion
+accordionHead.forEach((accordion) => {
+  accordion.addEventListener("click", function (e) {
+    const headDataId = accordion.dataset.id;
+    hidePreviousAccordionBody(headDataId);
+    if (accordion) {
+      console.log("jsh");
+    }
+    const nextSibling = accordion.nextElementSibling;
+    nextSibling.classList.toggle("hidden");
+  });
+});
+
+function hidePreviousAccordionBody(headDataId) {
+  accordionBody.forEach((item) => {
+    const bodyDataId = item.dataset.id;
+    if (headDataId !== bodyDataId) {
+      item.classList.add("hidden");
+    }
+  });
+}
+
+console.log(document.querySelector("input").checked);
